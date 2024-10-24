21dedb113d44b735dc9aa99e4bd84e4fd1caf332
Taehyun Lim <admin@limtaehyun.dev>
2024-10-24 11:56:26 +0900

    🔍️ Sitemap for naver

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteUrl: "https://sluurp.io",
   generateRobotsTxt: true,
-  sitemapSize: 7000,
+  sitemapSize: 500,
   changefreq: "daily",
   priority: 1,
   exclude: ["/workspaces/*", "/auth/*", "/workspaces/*"],
