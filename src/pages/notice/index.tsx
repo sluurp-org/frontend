376ad2660ec256b296f6db4d95e28dd3c0a0fc96
Taehyun Lim <admin@limtaehyun.dev>
2024-11-20 13:39:11 +0900

    🚑️ Idiot

--- src/pages/notice/index.tsx
+++ src/pages/notice/index.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Default() {
   const router = useRouter();
-  router.push(`/notice/14301e85453080828d67e19398b011a2`);
+
+  useEffect(() => {
+    router.push(`/notice/14301e85453080828d67e19398b011a2`);
+  }, [router]);
 
   return <></>;
 }
