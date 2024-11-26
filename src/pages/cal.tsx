3ca6c03b2887af0804e8cdca61152cefe9883780
Taehyun Lim <admin@limtaehyun.dev>
2024-11-26 09:06:49 +0900

    🐛 Remove channel talk in cal page

--- src/pages/cal.tsx
+++ src/pages/cal.tsx
@@ -1,5 +1,13 @@
 import Calendar from "@/components/Calendar";
+import { useChannel } from "@/contexts/ChannelContext";
+import { useEffect } from "react";
 
 export default function CalPage() {
+  const channelTalk = useChannel();
+
+  useEffect(() => {
+    channelTalk?.hideChannelButton();
+  });
+
   return <Calendar />;
 }
